75b7ccf3d128ad699be7d59941349947b9baae57
Your Name <you@example.com>
2021-03-30 14:33:09 -0400

    nik's update

--- R/core.R
+++ R/core.R
@@ -1,55 +1,64 @@
-# R script to align sequence reads to reference database and identify ancient DNA damage patterns 
+# R Script to Align Sequence Reads to Reference Database and Identify Ancient DNA Damage Patterns 
 
-# set up path to data 
-# set up path to reference
-source('R/paths.R')
-# fastq-dump example: 
-#fastq-dump SRR7774477
+# Set Up Path to Data 
+system('fastq-dump SRR7774472')    #### is there an R solution using NCBI Entrez tools or sratoolkit?
 
+# Getting the Reference Data
+download.file('http://ftp.ncbi.nlm.nih.gov/refseq/release/mitochondrion/mitochondrion.2.1.genomic.fna.gz', 'mito_reference.fna.gz')
+ref = 'mito_reference.fna' #change to chloroplast or bacterial?
+data='SRR7774472.fastq'
 
-# Consider overlapping reads and filtering for replicates before aligning with bwa
-
-#Quality control: low quality, adapter trimming, duplicates
-
-# align with bwa
+# Alignment with BWA
 index_bwa = paste('bwa index', ref)
 system(index_bwa) # index reference
-run_bwa = paste('bwa mem -Y -I 0 -L 1024 -E 7 -t 16', ref, data, '> align.sam')
-system(run_bwa) # run alignment
+run_bwa = paste('bwa mem -Y -I 0 -L 1024 -E 7 -t 32', ref, data, '> align.sam') 
+system(run_bwa) #Runs Alignment
 
-
-# samtools index and convert
-
+#Samtools Index and Convert
 run_samtools = paste(
-'samtools view -S -b align.sam > align.bam \n
-samtools view -b -F4 align.bam > align.final.bam \n
-samtools sort align.final.bam > align.sort.bam \n
-samtools index align.sort.bam \n')
+  'samtools view -S -b align.sam > align.bam \n
+  samtools view -b -F4 align.bam > align.final.bam \n
+  samtools sort align.final.bam > align.sort.bam \n
+  samtools index align.sort.bam \n') #bamtofastq this file?
 system(run_samtools)
 
-#### Set paths
-
+# Set Path
 system('git clone https://github.com/rsh249/PMDtools') #python3 compatible code?
 
-#pmdtools deamination mapping
-# run_pmdtools = paste('samtools calmd -b align.sort.bam', ref, '| samtools view -h - | python3 ../PMDtools/pmdtools.0.60.py --threshold 0 --header --deamination --range 125 --CpG > out.CpG')
-# system(run_pmdtools)
+#Deamination Mapping using PMDTools
+system('gunzip mito_reference.fna.gz')
+
+run_pmdtools = paste('samtools calmd -b align.sort.bam', ref, '| samtools view -h - | python3 ../PMDtools/pmdtools.0.60.py --threshold 0 --header --deamination --range 125 --CpG > out.CpG')
+system(run_pmdtools)
 run_pmdtools2 = paste('samtools view -h align.sort.bam | python3 ../PMDtools/pmdtools.0.60.py --deamination --range 125 > out2.deam') #as in Moore etal. 2020
 system(run_pmdtools2)
 
-
-#### FILTERING ######
-#pmdtools filtering on PMD score 0
+#Filtering on PMD Score of "0"
 run_pmdtools_filter = paste('samtools view -h align.sort.bam | python3 ../PMDtools/pmdtools.0.60.py --threshold 0 --header | samtools view -Sb - > filter.bam') 
 system(run_pmdtools_filter)
 
-# bam to fastq
-run_bam2fastq = paste('samtools fastq -@ 8 filter.bam -1 filter.R1.fastq -2 filter.R2.fastq')
+# Bam to Fastq Conversion
+run_bam2fastq = paste('samtools fastq -@ 8 filter.bam > filter.fastq')
 system(run_bam2fastq)
 
-# plot
+# Plotting (needs work)
 library(ShortRead)
 library(ggplot2)
+
+raw_kreport = data.table::fread('raw_kreport.tab')
+library(dplyr)
+library(ggplot2)
+filter_raw_kreport = raw_kreport %>%
+  filter(V1>=1) %>% filter(V4=='F')
+ggplot(filter_raw_kreport) + geom_col(aes(x=V6, y = V2)) + theme(axis.text.x = element_text(angle=90))
+
+filtered_kreport = data.table::fread('filtered_kreport.tab')
+library(dplyr)
+library(ggplot2)
+filter_filtered_kreport = filtered_kreport %>%
+  filter(V1>=1) %>% filter(V4=='F')
+ggplot(filter_filtered_kreport) + geom_col(aes(x=V6, y = V2)) + theme(axis.text.x = element_text(angle=90))
+
 filter_fastq = readFastq('.', pattern='*.fastq')
 filter_reads = sread(filter_fastq)
 filter_readlengths = filter_reads@ranges@width
@@ -63,7 +72,6 @@ ggplot(avgscores) +
   xlab('Quality Score') +
   ggtitle('Per Read Average Quality')
 
-# overlap reads and plot length distribution
 
-
+#overlap reads and plot length distribution
 #out2.deam
